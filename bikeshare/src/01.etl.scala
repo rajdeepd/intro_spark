8bd0190939b882392e6a5e4228074399ffa35d80
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-11-13 10:01:25 +0530

    modified etl script

--- bikeshare/src/01.etl.scala
+++ bikeshare/src/01.etl.scala
@@ -1,6 +1,7 @@
 // load bikeshare data set
 // www.capitalbikeshare.com/system-data
-val raw_trips = sc.textFile("2014-Q1-Trips-History-Data2.csv")
+val PATH="/home/ubuntu/work/spark/intro_spark/data/2014-Q1-cabi-trip-history-data.csv"
+val raw_trips = sc.textFile(PATH)
 raw_trips.take(4)
 
 def convertDur(dur: String): Long = {
@@ -8,7 +9,7 @@ def convertDur(dur: String): Long = {
   val dur_regex(hour, minute, second) = dur
   (hour.toLong * 3600L) + (minute.toLong * 60L) + second.toLong
 }
-
+//Duration,Start date,Start Station,End date,End Station,Bike#,Member Type
 case class Trip(id: String, dur: Long, s0: String, s1: String, reg: String)
 
 val bike_trips = raw_trips.map(_.split(",")).filter(_(0) != "Duration").
